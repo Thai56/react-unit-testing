a0f07c240d9361b113f14ff974305f2103812d81
Thai Tran <Thai56@Thais-MacBook-Air.local>
2017-03-02 12:54:26 -0700

    comment app working

---
+++ src/reducers/comments.js
@@ -0,0 +1,11 @@
+import { SAVE_COMMENT } from '../actions/types';
+const initialState = [];
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SAVE_COMMENT:
+      return [ ...state, action.payload ];
+  }
+
+ return state
+}
